b3cdd573dac86e72341d189f506e142d0dbef889
behnam <behnamnabati0@gmail.com>
2025-03-17 19:26:16 +0330

    next.config.js modified

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        appDir: true, // Enable experimental app directory support
-    },
-    async redirects() {
-        return [
-            {
-                source: '/old-route',
-                destination: '/new-route',
-                permanent: true,
-            },
-        ];
-    },
-    // Optional: Revalidate on a specific path to force reloading of content
-    revalidate: 0, // This will prevent caching issues with Vercel edge functions
-};
+const nextConfig = {};
 
 export default nextConfig;
