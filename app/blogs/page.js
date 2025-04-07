0d38985fdaba8b1e4fd24f874e6acce3dc630181
behnam-nbt <behnamnabati0@gmail.com>
2025-04-08 00:23:50 +0330

    blog page changed to ISR

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -3,6 +3,8 @@ import BlogPage from '@/components/template/BlogPage';
 import { fetchBlogs } from '@/services/fetchData'
 import React from 'react'
 
+export const revalidate = 3600;
+
 async function Blogs() {
     const blogs = await fetchBlogs();
     return (
